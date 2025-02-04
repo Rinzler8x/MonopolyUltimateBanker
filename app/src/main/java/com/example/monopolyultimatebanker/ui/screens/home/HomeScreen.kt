675cb5286a00ea3aad28e7f45e582debb87fbbe9
Varad Kakodkar <varad25gt@gmail.com>
2025-02-05 03:11:57 +0530

    Temporary code for testing FirestoreRepository.

--- app/src/main/java/com/example/monopolyultimatebanker/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/monopolyultimatebanker/ui/screens/home/HomeScreen.kt
@@ -1,12 +1,17 @@
 package com.example.monopolyultimatebanker.ui.screens.home
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.monopolyultimatebanker.ui.navigation.NavigationDestination
 
 object HomeDestination: NavigationDestination {
@@ -15,14 +20,23 @@ object HomeDestination: NavigationDestination {
 
 @Composable
 fun HomeScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = hiltViewModel()
 ) {
     Scaffold() { contentPadding ->
         Box(
             modifier = modifier.padding(contentPadding),
             contentAlignment = Alignment.Center
         ) {
-            Text("Home Page")
+            Column {
+                Text("Scan this card then any property card.\n(Rent level resets to 1)")
+                Spacer(modifier = modifier.padding(top = 50.dp))
+                Button(
+                    onClick = { viewModel.insertDataTest() }
+                ) {
+                    Text("run test")
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/monopolyultimatebanker/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/example/monopolyultimatebanker/ui/screens/home/HomeViewModel.kt
@@ -1,14 +1,23 @@
 package com.example.monopolyultimatebanker.ui.screens.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.monopolyultimatebanker.data.firebase.database.FirestoreRepositoryImpl
+import com.example.monopolyultimatebanker.data.gametable.Game
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-
+    private val firestoreRepositoryImpl: FirestoreRepositoryImpl
 ): ViewModel() {
 
-
+    fun insertDataTest() {
+        val game = Game("1", "Jason", 50)
+        viewModelScope.launch {
+            firestoreRepositoryImpl.updateGamePlayer(game)
+        }
+    }
 }
