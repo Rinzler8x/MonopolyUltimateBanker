7599da6f8a3c6d3bc1f3271939a5bf5ed3dac8d3
Varad Kakodkar <varad25gt@gmail.com>
2025-02-05 03:08:38 +0530

    @Inject annotation added.

--- app/src/main/java/com/example/monopolyultimatebanker/data/preferences/UserLoginPreferencesRepository.kt
+++ app/src/main/java/com/example/monopolyultimatebanker/data/preferences/UserLoginPreferencesRepository.kt
@@ -13,6 +13,7 @@ import com.example.monopolyultimatebanker.ui.screens.signupandlogin.UiState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
 data class UserLogin(
     val isLoggedIn: Boolean,
@@ -20,7 +21,7 @@ data class UserLogin(
     val email: String,
 )
 
-class UserLoginPreferencesRepository (
+class UserLoginPreferencesRepository @Inject constructor (
     private val dataStore: DataStore<Preferences>
 ){
     private companion object {
