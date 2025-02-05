b3eb10bfa43f99314cda1d058a2aa87d81b915e2
Varad Kakodkar <varad25gt@gmail.com>
2025-02-05 07:00:05 +0530

    Room database instance class created.

---
+++ app/src/main/java/com/example/monopolyultimatebanker/data/room/MonopolyDatabase.kt
@@ -0,0 +1,42 @@
+package com.example.monopolyultimatebanker.data.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.monopolyultimatebanker.data.eventtable.Event
+import com.example.monopolyultimatebanker.data.eventtable.EventDao
+import com.example.monopolyultimatebanker.data.gametable.Game
+import com.example.monopolyultimatebanker.data.gametable.GameDao
+import com.example.monopolyultimatebanker.data.playerpropertytable.PlayerProperty
+import com.example.monopolyultimatebanker.data.playerpropertytable.PlayerPropertyDao
+import com.example.monopolyultimatebanker.data.propertytable.Property
+import com.example.monopolyultimatebanker.data.propertytable.PropertyDao
+
+@Database(
+    entities = [Property::class, Event::class, Game::class, PlayerProperty::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class MonopolyDatabase: RoomDatabase() {
+
+    abstract fun propertyDao(): PropertyDao
+    abstract fun eventDao(): EventDao
+    abstract fun gameDao(): GameDao
+    abstract fun playerPropertyDao(): PlayerPropertyDao
+
+    companion object {
+        @Volatile
+        private var Instance: MonopolyDatabase? = null
+
+        fun getDatabase(context: Context): MonopolyDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, MonopolyDatabase::class.java, "monopoly_database")
+                    .fallbackToDestructiveMigration()
+                    .createFromAsset("database/monopoly_prepopulate.db")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
