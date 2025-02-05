5e1c5cb9a25d0bc816bf0239771efd8aeede2d4e
Varad Kakodkar <varad25gt@gmail.com>
2025-02-05 19:48:53 +0530

    Added necessary update query functions for game logic.

--- app/src/main/java/com/example/monopolyultimatebanker/data/playerpropertytable/PlayerPropertyDao.kt
+++ app/src/main/java/com/example/monopolyultimatebanker/data/playerpropertytable/PlayerPropertyDao.kt
@@ -12,6 +12,9 @@ interface PlayerPropertyDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(playerProperty: PlayerProperty)
 
+    @Query("SELECT * FROM player_property WHERE property_no = :propertyNo")
+    fun getPlayerProperty(propertyNo: Int): PlayerProperty
+
     @Query("UPDATE player_property SET player_id = :playerId WHERE property_no = :propertyNo")
     suspend fun swapProperty(playerId: Int, propertyNo: Int)
 
@@ -20,4 +23,63 @@ interface PlayerPropertyDao {
         swapProperty(player2Id, property1No)
         swapProperty(player1Id, property2No)
     }
+
+    @Query("UPDATE player_property SET rent_level = 1 WHERE property_no = :propertyNo")
+    suspend fun rentLevelReset1(propertyNo: Int)
+
+    @Query("UPDATE player_property SET rent_level = 5 WHERE property_no = :propertyNo")
+    suspend fun rentLevelJumpTo5(propertyNo: Int)
+
+    @Query("UPDATE player_property SET rent_level = rent_level + 1 WHERE property_no = :propertyNo AND rent_level != 5")
+    suspend fun rentLevelIncrease(propertyNo: Int)
+
+    @Query("UPDATE player_property SET rent_level = rent_level - 1 WHERE property_no = :propertyNo AND rent_level != 1")
+    suspend fun rentLevelDecrease(propertyNo: Int)
+
+    @Query(
+        "UPDATE player_property SET rent_level = rent_level - 1 " +
+        "WHERE property_no " +
+        "IN (SELECT property.property_no FROM property INNER JOIN player_property ON player_property.property_no = property.property_no " +
+        "WHERE property.property_no = :propertyNo - 1 OR property.property_no = :propertyNo + 1)"
+    )
+    suspend fun rentLevelDecreaseForNeighbors(propertyNo: Int)
+
+    @Transaction
+    suspend fun eventRentDecreaseForNeighbors(propertyNo: Int) {
+        rentLevelIncrease(propertyNo)
+        rentLevelDecreaseForNeighbors(propertyNo)
+    }
+
+    @Query(
+        "UPDATE player_property SET rent_level = rent_level + 1 " +
+        "WHERE property_no IN " +
+        "(SELECT property.property_no FROM property INNER JOIN player_property ON player_property.property_no = property.property_no " +
+        "WHERE board_side IN (SELECT board_side FROM property WHERE property_no = :propertyNo)) AND rent_level != 5"
+    )
+    suspend fun eventRentLevelIncreaseBoardSide(propertyNo: Int)
+
+    @Query(
+        "UPDATE player_property SET rent_level = rent_level - 1 " +
+        "WHERE property_no IN " +
+        "(SELECT property.property_no FROM property INNER JOIN player_property ON player_property.property_no = property.property_no " +
+        "WHERE board_side IN (SELECT board_side FROM property WHERE property_no = :propertyNo)) AND rent_level != 1"
+    )
+    suspend fun eventRentLevelDecreaseBoardSide(propertyNo: Int)
+
+    @Query(
+        "UPDATE player_property SET rent_level = rent_level + 1 " +
+        "WHERE property_no IN " +
+        "(SELECT property.property_no FROM property INNER JOIN player_property ON player_property.property_no = property.property_no " +
+        "WHERE color IN (SELECT color FROM property WHERE property_no = :propertyNo)) AND rent_level != 5"
+    )
+    suspend fun eventRentLevelIncreaseColorSet(propertyNo: Int)
+
+    @Query(
+        "UPDATE player_property SET rent_level = rent_level + 1 " +
+        "WHERE property_no IN " +
+        "(SELECT property.property_no FROM property INNER JOIN player_property ON player_property.property_no = property.property_no " +
+        "WHERE color IN (SELECT color FROM property WHERE property_no = :propertyNo)) AND rent_level != 5"
+    )
+    suspend fun eventRentLevelDecreaseColorSet(propertyNo: Int)
+
 }
