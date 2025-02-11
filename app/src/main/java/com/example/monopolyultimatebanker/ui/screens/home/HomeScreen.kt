a551c66115289e5aa3c5aaabf188aa51a625fd11
Varad Kakodkar <varad25gt@gmail.com>
2025-02-12 01:18:46 +0530

    Home screen navigation drawer setup.

--- app/src/main/java/com/example/monopolyultimatebanker/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/monopolyultimatebanker/ui/screens/home/HomeScreen.kt
@@ -1,17 +1,40 @@
 package com.example.monopolyultimatebanker.ui.screens.home
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.monopolyultimatebanker.R
 import com.example.monopolyultimatebanker.ui.navigation.NavigationDestination
 
 object HomeDestination: NavigationDestination {
@@ -23,20 +46,151 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = hiltViewModel()
 ) {
-    Scaffold() { contentPadding ->
-        Box(
-            modifier = modifier.padding(contentPadding),
-            contentAlignment = Alignment.Center
-        ) {
-            Column {
-                Text("Scan this card then any property card.\n(Rent level resets to 1)")
-                Spacer(modifier = modifier.padding(top = 50.dp))
-                Button(
-                    onClick = { viewModel.insertDataTest() }
-                ) {
-                    Text("run test")
-                }
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = viewModel.navDrawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                DrawerContent()
+            }
+        },
+        gesturesEnabled = viewModel.navDrawerState.isOpen
+    ) {
+        Scaffold(
+            topBar = {
+                GameTopAppBar(onClickNavIcon = { viewModel.onClickNavIcon(scope.coroutineContext) })
+            },
+            floatingActionButton = { ExpandableFloatingActionButton() }
+        ) { contentPadding ->
+            Column (
+                modifier = modifier.padding(contentPadding)
+            ) {
+                NoActiveGame()
             }
         }
     }
+
+}
+
+@Composable
+fun DrawerContent(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxWidth().padding(16.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Filled.AccountCircle,
+            contentDescription = "Account Icon",
+            modifier = modifier.size(120.dp)
+        )
+        Text("Username: Rinzler")
+        Text("Game ID: 123")
+        HorizontalDivider(modifier = modifier.padding(bottom = 12.dp, top = 8.dp))
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = modifier.fillMaxWidth()
+        ) {
+            Button(
+                onClick = {},
+                enabled = false
+            ) {
+                Text("Leave")
+            }
+            Button(
+                onClick = {},
+            ) {
+                Text("Log Out")
+            }
+        }
+    }
+}
+
+@Composable
+fun NoActiveGame(modifier: Modifier = Modifier) {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text("Create or Join Game")
+        HorizontalDivider(
+            modifier = modifier.width(220.dp).padding(vertical = 4.dp)
+        )
+    }
+}
+
+@Composable
+fun ExpandableFloatingActionButton(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+//        AnimatedVisibility(
+//            modifier = modifier.padding(bottom = 12.dp),
+//            visible = state.value,
+//            enter = slideInVertically {
+//                // Slide in from 40 dp from the top.
+//                with(density) { -40.dp.roundToPx() }
+//            } + expandVertically(
+//                // Expand from the top.
+//                expandFrom = Alignment.Top
+//            ) + fadeIn(
+//                // Fade in with the initial alpha of 0.3f.
+//                initialAlpha = 0.3f
+//            ),
+//            exit =  slideOutVertically(targetOffsetY = { fullHeight -> fullHeight }) + shrinkVertically() + fadeOut()
+//        ) {}
+        SmallFloatingActionButton (
+            onClick = { },
+        ) {
+            Icon(
+                Icons.Filled.Add,
+                contentDescription = stringResource(R.string.floating_action_button)
+            )
+        }
+        SmallFloatingActionButton (
+            onClick = { },
+        ) {
+            Icon(
+                Icons.Filled.Add,
+                contentDescription = stringResource(R.string.floating_action_button)
+            )
+        }
+        FloatingActionButton(
+            onClick = { },
+            modifier = modifier.padding(top = 4.dp),
+        ) {
+            Icon(
+                Icons.Filled.Add,
+                contentDescription = stringResource(R.string.floating_action_button)
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GameTopAppBar(
+    onClickNavIcon: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Text(stringResource(R.string.app_name), maxLines = 1, overflow = TextOverflow.Ellipsis)
+        },
+        navigationIcon = {
+            IconButton(
+                onClick = onClickNavIcon,
+            ) {
+                Icon(Icons.Filled.Menu, contentDescription = "Menu")
+            }
+        }
+    )
 }
--- app/src/main/java/com/example/monopolyultimatebanker/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/example/monopolyultimatebanker/ui/screens/home/HomeViewModel.kt
@@ -1,12 +1,18 @@
 package com.example.monopolyultimatebanker.ui.screens.home
 
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.monopolyultimatebanker.data.firebase.database.FirestoreRepositoryImpl
-import com.example.monopolyultimatebanker.data.gametable.Game
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 
 @HiltViewModel
@@ -14,10 +20,17 @@ class HomeViewModel @Inject constructor(
     private val firestoreRepositoryImpl: FirestoreRepositoryImpl
 ): ViewModel() {
 
-    fun insertDataTest() {
-        val game = Game("1", "Jason", 50)
+    var navDrawerState by mutableStateOf(DrawerState(initialValue = DrawerValue.Closed))
+        private set
+
+
+    fun onClickNavIcon(compositionCoroutineContext: CoroutineContext) {
         viewModelScope.launch {
-            firestoreRepositoryImpl.updateGamePlayer(game)
+            withContext(compositionCoroutineContext) {
+                navDrawerState.apply {
+                    if (isClosed) open() else close()
+                }
+            }
         }
     }
 }
