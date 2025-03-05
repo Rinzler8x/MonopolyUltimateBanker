e43d1323f12392b05a3391582638d406a2e12199
Varad Kakodkar <varad25gt@gmail.com>
2025-03-06 01:21:35 +0530

    Added default arguments to make it a no-argument constructor.

--- app/src/main/java/com/example/monopolyultimatebanker/data/propertytable/Property.kt
+++ app/src/main/java/com/example/monopolyultimatebanker/data/propertytable/Property.kt
@@ -8,14 +8,14 @@ import androidx.room.PrimaryKey
 data class Property(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "property_no") val propertyNo: Int = 1,
-    @ColumnInfo(name = "qr_code") val qrCode: String,
-    @ColumnInfo(name = "property_name") val propertyName: String,
-    @ColumnInfo(name = "rent_level_1") val rentLevel1: Int,
-    @ColumnInfo(name = "rent_level_2") val rentLevel2: Int,
-    @ColumnInfo(name = "rent_level_3") val rentLevel3: Int,
-    @ColumnInfo(name = "rent_level_4") val rentLevel4: Int,
-    @ColumnInfo(name = "rent_level_5") val rentLevel5: Int,
-    @ColumnInfo(name = "color") val color: String,
-    @ColumnInfo(name = "board_side") val boardSide: Int,
+    @ColumnInfo(name = "qr_code") val qrCode: String = "",
+    @ColumnInfo(name = "property_name") val propertyName: String = "",
+    @ColumnInfo(name = "rent_level_1") val rentLevel1: Int = 0,
+    @ColumnInfo(name = "rent_level_2") val rentLevel2: Int = 0,
+    @ColumnInfo(name = "rent_level_3") val rentLevel3: Int = 0,
+    @ColumnInfo(name = "rent_level_4") val rentLevel4: Int = 0,
+    @ColumnInfo(name = "rent_level_5") val rentLevel5: Int = 0,
+    @ColumnInfo(name = "color") val color: String = "",
+    @ColumnInfo(name = "board_side") val boardSide: Int = 0,
 )
 
