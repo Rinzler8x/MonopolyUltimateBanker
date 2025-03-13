35d078f9d9c44da9111aae0c37f9a803262b4bb0
Varad Kakodkar <varad25gt@gmail.com>
2025-03-14 04:03:50 +0530

    Base layout of the PropertyCard done.

--- app/src/main/java/com/example/monopolyultimatebanker/ui/screens/propertycard/PropertyCard.kt
+++ app/src/main/java/com/example/monopolyultimatebanker/ui/screens/propertycard/PropertyCard.kt
@@ -1,13 +1,17 @@
 package com.example.monopolyultimatebanker.ui.screens.propertycard
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -39,18 +44,23 @@ fun PropertyCard(
 
     Scaffold { innerPadding ->
         Column(
-            modifier = modifier.padding(innerPadding).fillMaxHeight(),
+            modifier = modifier
+                .padding(innerPadding)
+                .padding(20.dp)
+                .fillMaxHeight(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(64.dp)
+            verticalArrangement = Arrangement.spacedBy(54.dp)
         ) {
             Row(
-                modifier = modifier.background(Color.Blue).fillMaxWidth().height(50.dp),
+                modifier = modifier
+                    .background(Color(propertyState.color))
+                    .fillMaxWidth()
+                    .height(50.dp),
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
                     text = propertyState.propertyNo.toString(),
-                    modifier = modifier.background(color = Color.Red)
                 )
             }
 
@@ -58,38 +68,83 @@ fun PropertyCard(
                 text = propertyState.propertyName,
             )
 
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = modifier.fillMaxWidth().padding(horizontal = 20.dp)
-            ) {
-                Text(
-                    text = "Rent Level"
-                )
-                Text(
-                    text = "Rent"
-                )
+            Row {
+                Column(
+                    modifier = modifier.weight(.65f)
+                ) {
+                    Text(
+                        text = "Rent Level"
+                    )
+                }
+
+                Column(
+                    modifier = modifier.weight(.35f)
+                ) {
+                    Text(
+                        text = "Rent"
+                    )
+                }
             }
 
-            repeat(5) {
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = modifier.fillMaxWidth().padding(horizontal = 20.dp)
-                ) {
-                    Row {
-                        repeat(5) {
-                            Text(
-                                text = it.plus(1).toString(),
-                                modifier = modifier.padding(end = 16.dp)
-                            )
+            repeat(5) { firstLoop ->
+                Row {
+                    Column(
+                        modifier = modifier.weight(.65f)
+                    ) {
+                        Row(
+                            modifier = modifier.padding(horizontal = 6.dp),
+                        ) {
+                            repeat(5) { secondLoop ->
+                                Box(
+                                    modifier = if(firstLoop == secondLoop) {
+                                        modifier
+                                            .rotate(-45f)
+                                            .border(
+                                                width = 2.dp,
+                                                color = Color.Black,
+                                                shape = RoundedCornerShape(6.dp)
+                                            )
+                                            .background(Color.Black, RoundedCornerShape(6.dp))
+                                            .padding(8.dp)
+                                    } else {
+                                        Modifier
+                                            .rotate(-45f)
+                                            .border(
+                                                width = 2.dp,
+                                                color = Color.Black,
+                                                shape = RoundedCornerShape(6.dp)
+                                            )
+                                        .background(Color.White, RoundedCornerShape(6.dp))
+                                            .padding(8.dp)
+                                    },
+                                ) {
+                                    Text(
+                                        text = secondLoop.plus(1).toString(),
+                                        color = if(firstLoop == secondLoop) {
+                                            Color.White
+
+                                        } else {
+                                            Color.Black
+                                        },
+                                        modifier = modifier.rotate(45f)
+                                    )
+                                }
+                                Spacer(
+                                    modifier = modifier.padding(horizontal = 1.dp)
+                                )
+                            }
                         }
                     }
-
-                    //Dynamically change the field variable
-                    val fieldName = "rentLevel${it + 1}"
-                    val field: Field = propertyState.javaClass.getDeclaredField(fieldName)
-                    field.isAccessible = true // Make the field accessible
-                    val rentValue = field.get(propertyState) as Int
-                    Text(text = "$${rentValue}")
+                    Column(
+                        modifier = modifier.weight(.35f)
+                    ) {
+                        //Dynamically change the field variable
+                        val fieldName = "rentLevel${firstLoop + 1}"
+                        val field: Field = propertyState.javaClass.getDeclaredField(fieldName)
+                        field.isAccessible = true // Make the field accessible
+                        val rentValue = field.get(propertyState) as Int
+                        Text(text = "$${rentValue}")
+                    }
                 }
             }
 
