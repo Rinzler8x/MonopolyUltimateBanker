98f9e66625b867239b889c34ef97f01ee34fdf69
Varad Kakodkar <varad25gt@gmail.com>
2025-03-16 05:17:55 +0530

    Setup the base layout for the EventCard to display data from room db.

--- app/src/main/java/com/example/monopolyultimatebanker/ui/screens/eventcard/EventCard.kt
+++ app/src/main/java/com/example/monopolyultimatebanker/ui/screens/eventcard/EventCard.kt
@@ -1,12 +1,23 @@
 package com.example.monopolyultimatebanker.ui.screens.eventcard
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.monopolyultimatebanker.ui.navigation.NavigationDestination
@@ -27,9 +38,55 @@ fun EventCard(
     Scaffold { innerPadding ->
 
         Column(
-            modifier = modifier.padding(innerPadding)
+            modifier = modifier.padding(innerPadding).padding(horizontal = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(40.dp)
         ) {
-            Text(text = eventState.title)
+            Row(
+                modifier = modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(text = eventState.title)
+            }
+
+            Row(
+                modifier = modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = eventState.phrase,
+                    textAlign = TextAlign.Center
+                )
+            }
+
+            Row(
+                modifier = modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    Icons.Filled.Home,
+                    contentDescription = "Icon",
+                    modifier = modifier.size(100.dp)
+                )
+            }
+
+            Row(
+                modifier = modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = eventState.action,
+                    textAlign = TextAlign.Center
+                )
+            }
+
+            Button(
+                onClick = {}
+            ) {
+                Text(
+                    text = "Action"
+                )
+            }
         }
     }
 }
