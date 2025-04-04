2ec4fc5dd1760a00be54c390388052cbff970ef5
Varad Kakodkar <varad25gt@gmail.com>
2025-04-05 04:18:53 +0530

    Created the initial functions based of the EventCard in viewmodel. Also created the PlayerBottomSheet and it's necessary function.

--- app/src/main/java/com/example/monopolyultimatebanker/ui/screens/eventcard/EventCard.kt
+++ app/src/main/java/com/example/monopolyultimatebanker/ui/screens/eventcard/EventCard.kt
@@ -2,30 +2,47 @@ package com.example.monopolyultimatebanker.ui.screens.eventcard
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.monopolyultimatebanker.data.gametable.Game
 import com.example.monopolyultimatebanker.ui.navigation.NavigationDestination
+import com.example.monopolyultimatebanker.ui.screens.home.GameState
+import kotlinx.coroutines.CoroutineScope
 
 object EventCardDestination: NavigationDestination {
     override val route = "event_card"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EventCard(
     modifier: Modifier = Modifier,
@@ -34,59 +51,155 @@ fun EventCard(
 
     val eventState by eventCardViewModel.eventState.collectAsStateWithLifecycle()
     val qrPrefState by eventCardViewModel.qrPrefState.collectAsStateWithLifecycle()
+    val gameState by eventCardViewModel.gameState.collectAsStateWithLifecycle()
+    val playerBottomSheetState = eventCardViewModel.playerBottomSheetState
+    val sheetState = rememberModalBottomSheetState()
 
     Scaffold { innerPadding ->
-
         Column(
-            modifier = modifier.padding(innerPadding).padding(horizontal = 20.dp),
+            modifier = modifier
+                .padding(innerPadding)
+                .padding(horizontal = 20.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(40.dp)
         ) {
-            Row(
-                modifier = modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Text(text = eventState.title)
-            }
+            EventCardContent(
+                title = eventState.title,
+                phrase = eventState.phrase,
+                action = eventState.action,
+                onClickAction = eventCardViewModel::onCLickPlayerBottomSheet
+            )
 
-            Row(
-                modifier = modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = eventState.phrase,
-                    textAlign = TextAlign.Center
-                )
-            }
-
-            Row(
-                modifier = modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Icon(
-                    Icons.Filled.Home,
-                    contentDescription = "Icon",
-                    modifier = modifier.size(100.dp)
-                )
-            }
-
-            Row(
-                modifier = modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = eventState.action,
-                    textAlign = TextAlign.Center
-                )
-            }
-
-            Button(
-                onClick = {}
-            ) {
-                Text(
-                    text = "Action"
+            if (playerBottomSheetState.showBottomSheet) {
+                PlayerBottomSheet(
+                    onClickPlayerBottomSheet = eventCardViewModel::onCLickPlayerBottomSheet,
+                    sheetState = sheetState,
+                    innerPadding = innerPadding,
+                    game = gameState.gameState
                 )
             }
         }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun PlayerBottomSheet(
+    onClickPlayerBottomSheet: () -> Unit,
+    sheetState: SheetState,
+    innerPadding: PaddingValues,
+    game: List<Game>,
+    modifier: Modifier = Modifier
+) {
+    ModalBottomSheet(
+        onDismissRequest = {
+            onClickPlayerBottomSheet()
+        },
+        sheetState = sheetState,
+        modifier = modifier.fillMaxHeight().padding(innerPadding)
+    ) {
+        LazyColumn(
+            modifier = modifier
+                .fillMaxWidth()
+//                .padding(horizontal = 50.dp),
+        ) {
+            item {
+                Row(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 10.dp, horizontal = 12.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = "Player")
+                    Text(text = "Balance")
+                }
+            }
+            items(items = game.sortedByDescending { it.playerBalance }, key = { it.playerId }) {
+//                Row(
+//                    modifier = modifier
+//                        .fillMaxWidth()
+//                        .padding(vertical = 10.dp),
+//                    horizontalArrangement = Arrangement.SpaceBetween
+//                ) {
+//                    Text(text = it.playerName)
+//                    Text(text = it.playerBalance.toString())
+//                }
+//
+                TextButton(
+                    onClick = {
+                        println(it.playerName)
+                    },
+                ) {
+                    Row(
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 10.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = it.playerName,
+                            fontSize = 20.sp,
+                        )
+                        Text(text = it.playerBalance.toString(), fontSize = 20.sp)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun EventCardContent(
+    title: String,
+    phrase: String,
+    action: String,
+    onClickAction: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(text = title)
+    }
+
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = phrase,
+            textAlign = TextAlign.Center
+        )
+    }
+
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            Icons.Filled.Home,
+            contentDescription = "Icon",
+            modifier = modifier.size(100.dp)
+        )
+    }
+
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = action,
+            textAlign = TextAlign.Center
+        )
+    }
+
+    Button(
+        onClick = onClickAction
+    ) {
+        Text(
+            text = "Action"
+        )
     }
 }
--- app/src/main/java/com/example/monopolyultimatebanker/ui/screens/eventcard/EventCardViewModel.kt
+++ app/src/main/java/com/example/monopolyultimatebanker/ui/screens/eventcard/EventCardViewModel.kt
@@ -1,11 +1,21 @@
 package com.example.monopolyultimatebanker.ui.screens.eventcard
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SheetState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.monopolyultimatebanker.data.eventtable.Event
 import com.example.monopolyultimatebanker.data.eventtable.EventRepositoryImpl
+import com.example.monopolyultimatebanker.data.gametable.Game
+import com.example.monopolyultimatebanker.data.gametable.GameRepositoryImpl
 import com.example.monopolyultimatebanker.data.preferences.QrPreferencesRepository
 import com.example.monopolyultimatebanker.data.preferences.QrType
+import com.example.monopolyultimatebanker.ui.screens.home.GameState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.SharingStarted
@@ -13,11 +23,17 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+data class PlayerBottomSheetState(
+    val showBottomSheet: Boolean = false
+)
 
 @HiltViewModel
 class EventCardViewModel @Inject constructor(
     private val qrPreferencesRepository: QrPreferencesRepository,
+    private val gameRepositoryImpl: GameRepositoryImpl,
     private val eventRepositoryImpl: EventRepositoryImpl
 ): ViewModel() {
 
@@ -31,6 +47,17 @@ class EventCardViewModel @Inject constructor(
                 initialValue = QrType()
             )
 
+    val gameState: StateFlow<GameState> =
+        gameRepositoryImpl.getGameStream().map {
+            GameState(it)
+        }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = GameState()
+            )
+
+
     @OptIn(ExperimentalCoroutinesApi::class)
     val eventState: StateFlow<Event> = qrPrefState
         .flatMapLatest {
@@ -41,4 +68,118 @@ class EventCardViewModel @Inject constructor(
                 started = SharingStarted.WhileSubscribed(5_000),
                 initialValue = Event()
             )
+
+    /**Player Bottom Sheet*/
+    var playerBottomSheetState by mutableStateOf(PlayerBottomSheetState())
+
+    fun onCLickPlayerBottomSheet() {
+        playerBottomSheetState = playerBottomSheetState.copy(showBottomSheet = !playerBottomSheetState.showBottomSheet)
+    }
+
+    private fun swapProperty() {
+        viewModelScope.launch {
+
+        }
+    }
+
+    private fun rentLevelResetTo1() {
+        viewModelScope.launch {
+
+        }
+    }
+
+    private fun rentLevelJumpsTo5() {
+        viewModelScope.launch {
+
+        }
+    }
+
+    private fun rentLevelIncreaseForYouAndDecreaseForNeighbours() {
+        viewModelScope.launch {
+
+        }
+    }
+
+    private fun rentLevelIncreaseForBoardSide() {
+        viewModelScope.launch {
+
+        }
+    }
+
+    private fun rentLevelDecreaseForBoardSide() {
+        viewModelScope.launch {
+
+        }
+    }
+
+    private fun rentLevelIncreaseForColorSet() {
+        viewModelScope.launch {
+
+        }
+    }
+
+    private fun rentLevelDecreaseForColorSet() {
+        viewModelScope.launch {
+
+        }
+    }
+
+    private fun pay50PerPropertyOwned() {
+        viewModelScope.launch {
+
+        }
+    }
+
+    private fun rentLevel1For2RentPayments() {
+        viewModelScope.launch {
+
+        }
+    }
+
+    private fun collect200() {
+        viewModelScope.launch {
+
+        }
+    }
+
+    fun onClickAction() {
+        when(qrPrefState.value.event) {
+            "monoeve_1", "monoeve_9" -> {
+                swapProperty()
+            }
+            "monoeve_2", "monoeve_13", "monoeve_18" -> {
+                rentLevelResetTo1()
+            }
+            "monoeve_20" -> {
+                rentLevelJumpsTo5()
+            }
+            "monoeve_3", "monoeve_6", "monoeve_7", "monoeve_12", "monoeve_19" -> {
+                //TODO: navigate back to home screen
+            }
+            "monoeve_4", "monoeve_22" -> {
+                rentLevelIncreaseForYouAndDecreaseForNeighbours()
+            }
+            "monoeve_5" -> {
+                rentLevelIncreaseForBoardSide()
+            }
+            "monoeve_17" -> {
+                rentLevelDecreaseForBoardSide()
+            }
+            "monoeve_11", "monoeve_14" -> {
+                rentLevelIncreaseForColorSet()
+            }
+            "monoeve_16" -> {
+                rentLevelDecreaseForColorSet()
+            }
+            "monoeve_8" -> {
+                pay50PerPropertyOwned()
+            }
+            "monoeve_10" -> {
+                rentLevel1For2RentPayments()
+            }
+            "monoeve_15", "monoeve_21" -> {
+                collect200()
+            }
+        }
+    }
 }
