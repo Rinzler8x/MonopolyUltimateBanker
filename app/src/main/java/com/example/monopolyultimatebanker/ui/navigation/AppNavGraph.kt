1f29edbe786e63dbaada0e89fae2d44e6c333c7e
Varad Kakodkar <varad25gt@gmail.com>
2025-05-17 04:13:40 +0530

    StartDestination set to default production variant.

--- app/src/main/java/com/example/monopolyultimatebanker/ui/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/monopolyultimatebanker/ui/navigation/AppNavGraph.kt
@@ -44,7 +44,7 @@ fun AppNavHost(
         NavHost(
             navController = navController,
             startDestination = if(uiState.isLoggedIn!!) HomeDestination.route else SignUpAndLogInDestination.route,
-//            startDestination = EventCardDestination.route, //Statically set to bypass other screens, **FOR DEVELOPMENT PURPOSE ONLY**
+//            startDestination = PropertyCardDestination.route, //Statically set to bypass other screens, **FOR DEVELOPMENT PURPOSE ONLY**
             modifier = modifier
         ) {
             composable(route = SignUpAndLogInDestination.route) {
