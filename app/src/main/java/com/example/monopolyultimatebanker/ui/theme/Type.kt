5d878158c9357142c265e4b4b39d319b7e963cfa
Varad Kakodkar <varad25gt@gmail.com>
2025-05-17 01:54:05 +0530

    Setup the Typography TextStyles for the new fonts.

--- app/src/main/java/com/example/monopolyultimatebanker/ui/theme/Type.kt
+++ app/src/main/java/com/example/monopolyultimatebanker/ui/theme/Type.kt
@@ -2,33 +2,125 @@ package com.example.monopolyultimatebanker.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.monopolyultimatebanker.R
+
+val ITCKabelStd = FontFamily(
+    Font(R.font.itc_kabel_std_bold, FontWeight.Bold),
+    Font(R.font.itc_kabel_std_book, FontWeight.Normal),
+    Font(R.font.itc_kabel_std_demi, FontWeight.SemiBold),
+    Font(R.font.itc_kabel_std_medium, FontWeight.Medium),
+    Font(R.font.itc_kabel_std_ultra, FontWeight.ExtraBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    displayLarge = TextStyle(
+        fontFamily = ITCKabelStd,
+        fontWeight = FontWeight.Bold,
+        fontSize = 56.sp,
+        lineHeight = 64.sp,
+        letterSpacing = 1.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = ITCKabelStd,
+        fontWeight = FontWeight.Bold,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 1.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = ITCKabelStd,
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        letterSpacing = 1.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = ITCKabelStd,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 1.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = ITCKabelStd,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 1.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = ITCKabelStd,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 1.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = ITCKabelStd,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 1.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = ITCKabelStd,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 1.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = ITCKabelStd,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 1.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = ITCKabelStd,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 1.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = ITCKabelStd,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 1.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = ITCKabelStd,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 1.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = ITCKabelStd,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 1.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = ITCKabelStd,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 1.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = ITCKabelStd,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        letterSpacing = 1.sp
+    ),
 )
