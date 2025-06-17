0400a96ccdea90f1056a28dc30f92e1473f40814
Varad Kakodkar <varad25gt@gmail.com>
2025-06-18 04:04:08 +0530

    Added gameOverCount data member to GamePrefState data class to make sure game over dialog and asset value calculation happens only once.

--- app/src/main/java/com/example/monopolyultimatebanker/data/preferences/GamePreferencesRepository.kt
+++ app/src/main/java/com/example/monopolyultimatebanker/data/preferences/GamePreferencesRepository.kt
@@ -8,6 +8,7 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
@@ -17,7 +18,8 @@ import javax.inject.Inject
 data class GamePrefState(
     val gameId: String = "",
     val playerId: String = "",
-    val isGameActive: Boolean = false
+    val isGameActive: Boolean = false,
+    val gameOverCount: Int = 0
 )
 
 class GamePreferencesRepository @Inject constructor(
@@ -28,6 +30,7 @@ class GamePreferencesRepository @Inject constructor(
         val GAME_ID = stringPreferencesKey("game_id")
         val PLAYER_ID = stringPreferencesKey("player_id")
         val IS_GAME_ACTIVE = booleanPreferencesKey("is_game_active")
+        val GAME_OVER_COUNT = intPreferencesKey("game_over_count")
     }
 
     val gameState: Flow<GamePrefState> = dataStore.data
@@ -43,7 +46,8 @@ class GamePreferencesRepository @Inject constructor(
             GamePrefState(
                 gameId = pref[GAME_ID] ?: "",
                 playerId = pref[PLAYER_ID] ?: "",
-                isGameActive = pref[IS_GAME_ACTIVE] ?: false //Set true to avoid create game screen, **CHANGE BACK AFTER DEVELOPMENT**
+                isGameActive = pref[IS_GAME_ACTIVE] ?: false, //Set true to avoid create game screen, **CHANGE BACK AFTER DEVELOPMENT**
+                gameOverCount = pref[GAME_OVER_COUNT] ?: 0,
             )
         }
 
@@ -56,6 +60,12 @@ class GamePreferencesRepository @Inject constructor(
             pref[GAME_ID] = gameId
             pref[PLAYER_ID] = playerId
             pref[IS_GAME_ACTIVE] = isGameActive
+        }
+    }
+
+    suspend fun gameOverCountUpdate(countValue: Int) {
+        dataStore.edit { pref ->
+            pref[GAME_OVER_COUNT] = countValue
         }
     }
 
